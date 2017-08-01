6d0b096a81261261a606234e4346fd433c6819fe
raydive <raywassmas@gmail.com>
2017-08-02 01:58:37 +0900

    add logger

--- src/main/scala/com/github/raydive/Abc.scala
+++ src/main/scala/com/github/raydive/Abc.scala
@@ -4,6 +4,7 @@ import com.github.raydive.metrics.RubyLikeAbcMetric
 import org.json4s.JsonAST.{JNothing, JObject}
 import org.json4s.{DefaultFormats, Formats}
 import org.scalatra.json._
+import org.slf4j.LoggerFactory
 
 case class Topic(id: Long,
                  name: String,
@@ -39,6 +40,8 @@ case class AbcResponse(message: String, replyTo: Long)
 class Abc extends WhatsUpStack with JacksonJsonSupport {
   protected implicit val jsonFormats: Formats = DefaultFormats
 
+  val logger = LoggerFactory.getLogger(getClass)
+
   before() {
     contentType = formats("json")
   }
@@ -46,8 +49,9 @@ class Abc extends WhatsUpStack with JacksonJsonSupport {
   post("/") {
     parsedBody match {
       case JNothing => halt(400, "Invalid Json")
-      case _: JObject => {
+      case json: JObject => {
         val req = parsedBody.extract[JsonRequest]
+        logger.info(req.post.message)
         RubyLikeAbcMetric.calculate(req.post.message) match {
           case Right(result) =>
             AbcResponse(s"Abc metric: $result", req.post.account.id)
